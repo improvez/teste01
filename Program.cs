3ab4c16a58b93b9f769a771f105d538b5ccec603
Lucas Abati Zanotto <386188@unoesc.lan>
2024-03-25 22:06:48 -0300

    add: ensinamento do final da aula dia 25/03

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using System;
+using _teste01.Models;
 
 System.Console.WriteLine("hello23");
+
+// Primeira Clinica
+
+Clinica c1 = new Clinica();
+Clinica.InstanceCount = 1;
+c1.ObjectCount = 1;
+
+// Segunda Clinica
+Clinica c2 = new Clinica();
+Clinica.InstanceCount++;
+c2.ObjectCount = 10;
+
+System.Console.WriteLine($"Valor C1: {c1.ObjectCount}");
+System.Console.WriteLine($"Instance Count: {Clinica.InstanceCount}");
+System.Console.WriteLine($"Valor C2: {c2.ObjectCount}");

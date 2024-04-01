83189c2103c23fb25e957d660d6d15bb16c18fc5
Lucas Zanotto <lucasabatizanotto@gmail.com>
2024-04-01 20:19:28 -0300

    sla

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿System.Console.WriteLine("hello2");
+﻿namespace _teste01 {
+    public class Customer {
+        public string Name {get;set;}
+        public string EmailAddress {get;set;}
+        public string HomeAddress {get;set;}
+        public string WorkAddress {get;set;}
+
+        public bool Validate() {
+            return true;
+        }
+
+        public Customer Retrieve() {
+            return new Customer();
+        }
+
+        public void Save(Customer customer) {
+            
+        }
+
+    }
+}
